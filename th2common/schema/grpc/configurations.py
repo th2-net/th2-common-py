ef147161557007fe9791ce014c461695581ff418
ivan.chadin <ivan.chadin@exactprosystems.com>
2020-08-10 15:15:57 +0400

    TH2-112 Infra schema
    Fix GrpcRouterConfiguration

--- th2common/schema/grpc/configurations.py
+++ th2common/schema/grpc/configurations.py
@@ -24,6 +24,19 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+#
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 # # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -55,9 +68,10 @@ class GrpcServerConfiguration(GrpcEndpointConfiguration):
 
 
 class GrpcRouterConfiguration(Configuration):
-    def __init__(self, services, server) -> None:
+    def __init__(self, services, server=None) -> None:
         self.services = services
-        self.serverConfiguration = GrpcServerConfiguration(**server)
+        if server is not None:
+            self.serverConfiguration = GrpcServerConfiguration(**server)
 
 
 class GrpcRouterFilterConfiguration(RouterFilter):
