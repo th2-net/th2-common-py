3a74b93310e329edb74e7024a3d9a5c2be1b6203
ivan.chadin <ivan.chadin@exactprosystems.com>
2020-08-10 14:52:58 +0400

    TH2-112 Infra schema
    Update proto and gen service

--- th2common/gen/act_fix_service.py
+++ th2common/gen/act_fix_service.py
@@ -12,29 +12,6 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-# # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 from . import act_fix_pb2_grpc as importStub
 
@@ -47,19 +24,19 @@ class ActService(object):
         return self.connector.create_request("placeOrderFIX",request)
 
     def sendMessage(self, request):
-        return self.connector.createRequest("sendMessage",request)
+        return self.connector.create_request("sendMessage",request)
 
     def placeQuoteRequestFIX(self, request):
-        return self.connector.createRequest("placeQuoteRequestFIX",request)
+        return self.connector.create_request("placeQuoteRequestFIX",request)
 
     def placeQuoteFIX(self, request):
-        return self.connector.createRequest("placeQuoteFIX",request)
+        return self.connector.create_request("placeQuoteFIX",request)
 
     def placeOrderMassCancelRequestFIX(self, request):
-        return self.connector.createRequest("placeOrderMassCancelRequestFIX",request)
+        return self.connector.create_request("placeOrderMassCancelRequestFIX",request)
 
     def placeQuoteCancelFIX(self, request):
-        return self.connector.createRequest("placeQuoteCancelFIX",request)
+        return self.connector.create_request("placeQuoteCancelFIX",request)
 
     def placeQuoteResponseFIX(self, request):
-        return self.connector.createRequest("placeQuoteResponseFIX",request)
+        return self.connector.create_request("placeQuoteResponseFIX",request)
