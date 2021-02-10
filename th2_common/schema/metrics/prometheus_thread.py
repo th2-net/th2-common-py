890e551aae72e23493588b5ea7fddda6a410cfdd
ivan.chadin <ivan.chadin@exactprosystems.com>
2021-02-10 21:06:13 +0400

    [TH2-1390] Update pika version

--- th2_common/schema/metrics/prometheus_thread.py
+++ th2_common/schema/metrics/prometheus_thread.py
@@ -1,30 +1,24 @@
-import asyncio
-import logging
 import threading
 
 from prometheus_client.exposition import start_wsgi_server
 
 from th2_common.schema.metrics.common_metrics import CommonMetrics
 
-logger = logging.getLogger()
-
 
 class PrometheusThread(threading.Thread):
+    stopped = False
 
     def __init__(self, port=8000, host='localhost'):
         threading.Thread.__init__(self)
         self.port = port
         self.host = host
-        self.__loop = asyncio.get_event_loop()
 
     def run(self):
         CommonMetrics.LIVENESS.inc()
         start_wsgi_server(self.port, self.host)
-        logger.info('Prometheus server is started')
-        self.__loop.run_forever()
+        while not self.stopped:
+            pass
 
     def stop(self):
-        try:
-            CommonMetrics.LIVENESS.dec()
-        finally:
-            self.__loop.close()
+        CommonMetrics.LIVENESS.dec()
+        self.stopped = True
