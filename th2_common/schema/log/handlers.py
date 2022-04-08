057b2d4df2dcb569d8a46c857abe66d72e4e05f4
vyacheslav.svirkov <vyacheslav.svirkov@exactprosystems.com>
2022-04-08 14:14:05 +0300

    [TH2-3425] Added handler to log config file.

--- th2_common/schema/log/handlers.py
+++ th2_common/schema/log/handlers.py
@@ -5,7 +5,7 @@ from prometheus_client import Counter
 
 class MetricLogHandler(logging.Handler):
 
-    def __init__(self, level='NOTSET', metric_name='th2_logging_metric'):
+    def __init__(self, level='ERROR', metric_name='th2_logging_metric'):
         self.metric = Counter(metric_name, 'This metric registers log entries below specified level',
                               ['logger', 'level'])
         super().__init__(level)
