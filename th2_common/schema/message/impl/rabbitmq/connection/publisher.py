b545c926b2f92ad806a95bcb9b10e1869af08f8a
butbaiak <konstantine.butbaia@exactprosystems.com>
2022-10-08 15:32:11 +0400

    [TH2-2552] fixing mypy and flake8 errors

--- th2_common/schema/message/impl/rabbitmq/connection/publisher.py
+++ th2_common/schema/message/impl/rabbitmq/connection/publisher.py
@@ -139,7 +139,10 @@ class Publisher:
         return list(filter(lambda x: x['name'] in destination_queues, self.get_queues()))
 
     def queues_message_count(self, routing_key: str, unacked: bool = True, ready: bool = True) -> int:
-        return sum([ready * queue['messages_ready'] + unacked * queue['messages_unacknowledged'] for queue in self.get_queues_info(routing_key)])
+        return sum([
+                ready * queue['messages_ready'] + 
+                unacked * queue['messages_unacknowledged'] 
+                    for queue in self.get_queues_info(routing_key)])
 
     def publish_message(self,
                         exchange_name: str,
